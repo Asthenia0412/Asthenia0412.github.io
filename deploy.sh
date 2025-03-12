338d54f40ddbcee38819c8e460d32837fb63fffe
xiaoyongcai <2283216402@qq.com>
2025-03-12 15:12:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Asthenia0412/Asthenia0412.github.io.git master:gh-pages
+git push -f git@github.com:Asthenia0412/Asthenia0412.github.io.git master
 
 cd -
